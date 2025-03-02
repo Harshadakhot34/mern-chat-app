529fa544240426232bf1d6f9c027c41acbdfbdca
Harshadakhot34 <hkhot30@gmail.com>
2025-03-03 00:12:51 +0530

    Project setup

---
+++ chat-app/src/home/Left/Logout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Left.css'
+import { CiLogout } from "react-icons/ci";
+
+const Logout = () => {
+  return (
+    <div className='px-4 pt-2'>
+<CiLogout className='Logout-icon' />    </div>
+  )
+}
+
+export default Logout
---
+++ chat-app/src/home/Right/Right.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Right.css'
+const Right = () => {
+  return (
+    <div className='Right_container
+    '>
+     right 
+    </div>
+  )
+}
+
+export default Right
